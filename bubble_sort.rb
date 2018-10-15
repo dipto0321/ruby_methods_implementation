960bb0f5e61b9f43a18607c76aa59ac3a0808c91
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-15 11:43:49 +0800

    Add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,4 +11,16 @@ class Array
     end
     self.clear.concat(sorted)
   end
+
+  def bubble_sort_by
+    sorted = []
+    1.upto(self.size) do
+      last_el = self.reduce block_given? ? {|x,y| yield(x,y) > 0 ? x : y} : self.reduce { |x, y| x > y ? x : y }
+      break if last_el.nil?
+
+      sorted.unshift(last_el)
+      self.delete(last_el)
+    end
+    self.clear.concat(sorted)
+  end
 end
