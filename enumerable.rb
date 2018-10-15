d17eaf0a5f575940aa36825eb57f79c6fc3aedfe
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-15 10:21:09 +0600

    complete my_each and my_each-with_index

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,16 @@
 module Enumerable
-  
+  def my_each
+    for i in 0...self.size
+      self.class == Hash ? yield(self.keys[i], self.values[i],i) : yield(self[i],i)
+    end
+  end
 end
+
+# a = {
+#   :apple => "hello",
+#   :orange => "hi"
+# }
+# b = [5,4,7,9,8,6]
+
+# a.my_each {|key , value| puts "#{key} : #{value}"}
+# b.my_each {|value| puts "#{value}"}
