715daedca28527ce5e5e241975d9b446d782c856
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-15 09:44:08 +0600

    add bubble_sort_by using yeild

--- bubble.rb
+++ bubble.rb
@@ -1,4 +1,4 @@
-def bubble(numbers)
+def bubble_sort(numbers)
   i,j,len = 0, 0, numbers.length
   while i<len do
     j=0
@@ -10,3 +10,20 @@ def bubble(numbers)
   end
  return numbers
 end
+
+def bubble_sort_by(numbers)
+  i,j,len = 0, 0, numbers.length
+  while i<len do
+    j=0
+    while j < len-i-1 do
+      numbers[j] , numbers[j+1] = numbers[j+1] , numbers[j] if yield(numbers[j],numbers[j+1]) > 0
+      j += 1
+    end
+    i+=1
+  end
+ return numbers
+end
+
+p bubble_sort_by(["hi", "hey", "hello"]) {|left, right| left.length - right.length}
+animals = %w[dog eagle zebra tiger cat]
+p bubble_sort_by(animals) {|left, right| left.length - right.length}
