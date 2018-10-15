0de5edf586915998394a73015a4fff75da5f748c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-15 16:11:43 +0800

    Add hash example to my_map

--- enumerable.rb
+++ enumerable.rb
@@ -113,10 +113,12 @@ b = [5,4,7,9,8,6]
 product = Proc.new {|n| n * 2}
 sum = Proc.new {|n| n + 2}
 power = Proc.new{|x| x**2}
-
+cool = Proc.new{|k,v| "#{v} is a cool #{k} language"}
  
 test = [1,2,3,4,5,6]
-
+languages = {:frontend => "JavaScript" , :backend => "Ruby"}
 p test.my_map(&product).my_map {|i| i**2}
 p test.my_map(&sum)
 p test.my_map(&power)
+puts "\n\n"
+print languages.my_map(&cool)
