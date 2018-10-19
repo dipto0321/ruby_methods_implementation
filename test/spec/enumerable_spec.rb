1f546d9689c0ade0729341b0c0a94b03c20a69f1
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-19 14:18:31 +0600

    add a test example for my_select

--- test/spec/enumerable_spec.rb
+++ test/spec/enumerable_spec.rb
@@ -3,6 +3,11 @@ require_relative "../../enumerable"
 RSpec.describe Enumerable do
   numbers = [1,2,3,4,5]
   disney = {:name => "Mickey", :type => "mouse"}
+  describe "#my_select" do
+    it "return even numbers" do
+      expect(numbers.my_select { |value| value % 2 == 0}).to eql([2,4])
+    end
+  end
   describe "#my_all?" do
     it "returns false if not all numbers are even" do
       expect(numbers.my_all? {|num| num % 2 == 0}).to eql(false)
@@ -11,6 +16,5 @@ RSpec.describe Enumerable do
     it "returns true if all keys of a hash is of class Symbol" do
       expect(disney.my_all? {|key, value| key.is_a? Symbol}).to eql(true)
     end
-
   end
 end
