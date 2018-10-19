a0a7046913ab6ed9e0fe5852483c9e8cfcf7fa74
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-10-19 15:49:07 +0800

    Build test structure
    - Create test directory
    - Initialize rspec
    - Create enumerable_spec.rb inside spec/
    - Create initial test for my_all?

---
+++ test/spec/enumerable_spec.rb
@@ -0,0 +1,16 @@
+require_relative "../../enumerable"
+# Testing for our own enumerable implementation
+RSpec.describe Enumerable do
+  numbers = [1,2,3,4,5]
+  disney = {:name => "Mickey", :type => "mouse"}
+  describe "#my_all?" do
+    it "returns false if not all numbers are even" do
+      expect(numbers.my_all? {|num| num % 2 == 0}).to eql(false)
+    end
+
+    it "returns true if all keys of a hash is of class Symbol" do
+      expect(disney.my_all? {|key, value| key.is_a? Symbol}).to eql(true)
+    end
+
+  end
+end
